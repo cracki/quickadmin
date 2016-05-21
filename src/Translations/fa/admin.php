488e76e121a22a06d54704c44595907bee0c2441
cracki <cracki@gmail.com>
2016-05-21 11:10:49 +0430

    add lang files and css files

---
+++ src/Translations/fa/admin.php
@@ -0,0 +1,85 @@
+<?php
+
+return [
+
+    // dashboard
+    'dashboard-title'                       => 'به داشبورد مدیریت خود خوش آمدید',
+
+    // partials-header
+    'partials-header-title'                 => 'کنترل پنل',
+
+    // partials-sidebar
+    'partials-sidebar-menu'                 => 'منو',
+    'partials-sidebar-users'                => 'کاربران',
+    'partials-sidebar-roles'                => 'سطوح دسترسی',
+    'partials-sidebar-user-actions'         => 'فعالیت کاربران',
+    'partials-sidebar-logout'               => 'خروج',
+
+    // partials-topbar
+    'partials-topbar-title'                 => 'کنترل پنل',
+
+    // users-create
+    'users-create-create_user'              => 'ساخت کاربر',
+    'users-create-name'                     => 'نام',
+    'users-create-name_placeholder'         => 'نام',
+    'users-create-email'                    => 'ایمیل',
+    'users-create-email_placeholder'        => 'ایمیل',
+    'users-create-password'                 => 'رمز عبور',
+    'users-create-password_placeholder'     => 'رمزعبور',
+    'users-create-role'                     => 'سطح دسترسی',
+    'users-create-role'                     => 'سطح دسترسی',
+    'users-create-btncreate'                => 'ساخت',
+
+    // users-edit
+    'users-edit-edit_user'                  => 'ویرایش کاربر',
+    'users-edit-name'                       => 'نام',
+    'users-edit-name_placeholder'           => 'نام',
+    'users-edit-email'                      => 'ایمیل',
+    'users-edit-email_placeholder'          => 'ایمیل',
+    'users-edit-password'                   => 'رمزعبور',
+    'users-edit-password_placeholder'       => 'رمزعبور',
+    'users-edit-role'                       => 'دسترسی',
+    'users-edit-btnupdate'                  => 'به روز رسانی',
+
+    // users-index
+    'users-index-add_new'                   => 'جدیئ',
+    'users-index-users_list'                => 'لیست کاربران',
+    'users-index-name'                      => 'نام',
+    'users-index-edit'                      => 'ویرایش',
+    'users-index-delete'                    => 'حذف',
+    'users-index-are_you_sure'              => 'اطمینان دارید',
+    'users-index-no_entries_found'          => 'موردی یافت نشد',
+
+    // users-controller
+    'users-controller-successfully_created' => 'کاربر با موفقیت ساخته شد',
+    'users-controller-successfully_updated' => 'کاربر با موفقیت به روز شد',
+    'users-controller-successfully_deleted' => 'کاربر با موفقیت حذف شد',
+
+    // roles-index
+    'roles-index-add_new'                   => 'جدید',
+    'roles-index-roles_list'                => 'لیست دسترسی ها',
+    'roles-index-title'                     => 'عنوان',
+    'roles-index-edit'                      => 'ویرایش',
+    'roles-index-delete'                    => 'حذف',
+    'roles-index-are_you_sure'              => 'اطمینان دارید؟',
+    'roles-index-no_entries_found'          => 'موردی یافت شند',
+
+    // roles-create
+    'roles-create-create_role'              => 'ساخت دسترسی',
+    'roles-create-title'                    => 'عنوان',
+    'roles-create-title_placeholder'        => 'عنوان',
+    'roles-create-btncreate'                => 'ساختن',
+
+    // roles-edit
+    'roles-edit-edit_role'                  => 'ویرایش دسترسی',
+    'roles-edit-title'                      => 'عنوان',
+    'roles-edit-title_placeholder'          => 'عنوان',
+    'roles-edit-btnupdate'                  => 'به روزر سانی',
+
+    // roles-controller
+    'roles-controller-successfully_created' => 'دسترسی با موفقیت ساخته شد',
+    'roles-controller-successfully_updated' => 'دسترسی با موفقیت به روز شد',
+    'roles-controller-successfully_deleted' => 'دسترسی با موفقیت حذف شد',
+
+];
+
